705fab2ff871c7da509d62f1d984da7306711bdd
Lucas Gaspar <gasparlucas8@gmail.com>
2023-09-04 21:51:24 -0300

    JS-2 Finalizado

--- JS-2/main.js
+++ JS-2/main.js
@@ -4,17 +4,54 @@ const createTask = (evento) => {
     evento.preventDefault()
     const input = document.querySelector("[data-form-input]");
     const value = input.value
-    const task = document.querySelector('[data-task]')
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement('li');
+    task.classList.add('card')
     input.value = ''
-    const content = `
-    <div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content
-    console.log(content);
+
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value
+    taskContent.appendChild(titleTask);
+
+    // task.innerHTML = content
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon())
+    list.appendChild(task);
 
 }
 
 btn.addEventListener('click', createTask)
+
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far');
+    i.classList.add('fa-check-square');
+    i.classList.add('icon');
+    i.addEventListener('click', completeTask);
+    return i;
+}
+
+const completeTask = (event) => {
+  const element = event.target;
+  element.classList.toggle('fas')
+  element.classList.toggle('completeIcon')
+  element.classList.toggle('far')
+}
+
+const deleteIcon = () => {
+  const content = `
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+  i.addEventListener('click', deleteTask)
+  return i
+}
+
+const deleteTask = (event) => {
+  console.log('eliminar tarea')
+  event.target.parentElement.remove()
+
+}
